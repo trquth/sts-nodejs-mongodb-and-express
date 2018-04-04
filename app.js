bf7177a1fd70dcc43fe62d9a219bc3721956c3bd
trquth <tranquthien@gmail.com>
2018-04-04 16:12:56 +0700

    Write, call and debug simple APIs

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
 var express = require('express')
 
-var handlebars = require('express3-handlebars').create({defaultLayout : 'main'})
+var handlebars = require('express3-handlebars').create({defaultLayout: 'main'})
 var app = express()
 
-app.set('port' , process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3000)
 app.engine('handlebars', handlebars.engine)
 app.set('view engine', 'handlebars')
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req , res ) => {
+var fortunes = ["Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",];
+
+var tours = [{id: 0, name: 'Hood River', price: 99.99},
+{id: 1, name: 'Oregon Coast', price: 149.95},];
+
+app.get('/', (req, res) => {
     res.render('home')
 })
 
 app.get('/about', (req, res) => {
-    res.render('about')
+    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', {fortune: randomFortune})
 })
 
-app.use((req, res) =>{
+app.get('/api/tours', (req, res) => {
+    res.json(tours)
+})
+
+app.get('/api/tour/:id', (req, res) => {
+    var p = tours.some((item) => item.id == req.params.id)
+    if (p) {
+        res.json({success : true})
+    }else{
+        res.json({err : `Can't found item`})
+    }
+})
+
+app.use((req, res) => {
     res.render('400')
 })
 
@@ -27,7 +50,7 @@ app.use((req, res) => {
 
 
 
-app.listen(app.get('port'), () =>{
+app.listen(app.get('port'), () => {
     console.log(`Express started on 
     http://localhost:${app.get('port')};
     press Ctrl-C to terminate`)
