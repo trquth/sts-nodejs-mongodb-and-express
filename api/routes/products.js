6bdcbaac6e382a37a9c789065e6d5e8443bbb8e3
thientran <thien.tran@saigontechnology.vn>
2018-04-05 23:57:33 +0700

    Starting APIs NodeJS

---
+++ api/routes/products.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message : 'Handling GET requests to /products'
+    })
+})
+
+router.post('/', (req, res, next) => {
+    const product = {
+        name : req.body.name,
+        price : req.body.price
+    }
+    res.status(201).json({
+        message : 'Handling POST requests to /products',
+        data : product
+    })
+})
+
+router.get('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message : 'Received productId',
+        id : req.params.productId
+    })
+})
+
+router.put('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message : 'Updated product'
+    })
+})
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message : 'Deleted product'
+    })
+})
+
+module.exports = router
