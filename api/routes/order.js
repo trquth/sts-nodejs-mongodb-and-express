e6a795c808ce40c6ec745f749f2e5dbedd9f0005
trquth <tranquthien@gmail.com>
2018-04-13 07:43:21 +0700

    Populate data

--- api/routes/order.js
+++ api/routes/order.js
@@ -8,6 +8,7 @@ const Product = require('../models/product')
 router.get('/', (req, res, next) => {
     Order.find()
         .select("product quantity _id")
+        .populate('product', 'name')
         .exec()
         .then(docs => {
             res.status(200).json({
@@ -72,17 +73,48 @@ router.post('/', (req, res, next) => {
 });
 
 router.get('/:orderId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Order details',
-        orderId: req.params.orderId
-    });
+    Order.findById(req.params.orderId)
+        .populate('product','name')
+        .exec()
+        .then(order => {
+            if (!order) {
+                return res.status(404).json({
+                    message: "Order not found"
+                })
+            }
+
+            res.status(200).json({
+                order: order,
+                request: {
+                    type: "GET",
+                    url: "http://localhost:3000/orders"
+                }
+            });
+        }).catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
 });
 
 router.delete('/:orderId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Order deleted',
-        orderId: req.params.orderId
-    });
+    Order.remove({_id : req.params.orderId})
+    .exec()
+    .then(result => {
+        res.status(200).json({
+            message: "Order deleted",
+            request: {
+              type: "POST",
+              url: "http://localhost:3000/orders",
+              body: { productId: "ID", quantity: "Number" }
+            }
+          });
+    })
+    .catch(err => {
+        res.status(500).json({
+            error : err
+        })
+    })
 });
 
 module.exports = router
