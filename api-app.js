e96606324666801b7be3303822da13eefb2f137e
trquth <tranquthien@gmail.com>
2018-04-13 08:20:30 +0700

    Using multer to upload image to server

--- api-app.js
+++ api-app.js
@@ -8,6 +8,7 @@ const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/order')
 
 app.use(morgan('dev'))
+app.use('/uploads',express.static('uploads'))
 app.use(bodyParser.urlencoded({extended : false}))
 app.use(bodyParser.json())
 
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,14 +1,59 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, './uploads/')
+    },
+    filename: (req, file, callback) => {
+        callback(null, new Date().toISOString() + file.originalname)
+    }
+})
+
+const fileFilter = (req, file, callback) => {
+    file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' ? callback(null, true) : callback(new Error('The file type is incorrectly'), false)
+
+}
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter : fileFilter
+})
 
 const Product = require('./../models/product')
 
 router.get('/', (req, res, next) => {
     Product.find()
+        .select('name price productImage')
         .exec()
         .then(docs => {
-            res.status(200).json(docs)
+            const response = {
+                count: docs.length,
+                products: docs.map(doc => {
+                  return {
+                    name: doc.name,
+                    price: doc.price,
+                    productImage: doc.productImage,
+                    _id: doc._id,
+                    request: {
+                      type: "GET",
+                      url: "http://localhost:3000/products/" + doc._id
+                    }
+                  };
+                })
+              };
+              //   if (docs.length >= 0) {
+              res.status(200).json(response);
+              //   } else {
+              //       res.status(404).json({
+              //           message: 'No entries found'
+              //       });
+              //   }
         })
         .catch(err => {
             res.status(500).json({
@@ -17,11 +62,13 @@ router.get('/', (req, res, next) => {
         })
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', upload.single('productImage'), (req, res, next) => {
+    console.log('IMAGE', req.file)
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
-        price: req.body.price
+        price: req.body.price,
+        productImage: req.file.path
     })
     product.save().then(result => {
         console.log('RESULT--->', result)
@@ -77,16 +124,16 @@ router.patch('/:productId', (req, res, next) => {
 
 router.delete('/:productId', (req, res, next) => {
     const id = req.params.productId
-    Product.remove({_id : id})
-    .exec()
-    .then(result =>{
-        res.status(200).json('result')
-    })
-    .catch(err => {
-        res.status(500).json({
-            error : err
+    Product.remove({_id: id})
+        .exec()
+        .then(result => {
+            res.status(200).json('result')
         })
-    })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
 })
 
 module.exports = router
